c677df24db111cb840f771f8dec7dd9e40ed193d
Jessica Marshall <jessica.marshall14@gmail.com>
2017-06-09 09:39:36 -0400

    initial commit

---
+++ NumberGame.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.8;
+
+//everyone stores their own number once
+//can get someone else's number
+//add that number to your number
+
+contract NumberGame{
+
+    mapping(address => uint) public Numbers;
+
+    function NumberGame(){        //initialize contract
+    }
+
+    function storeNumber(uint myNum){
+        if(Numbers[msg.sender] != 0) throw;  //if you already stored, throw
+        Numbers[msg.sender] == myNum;    //store your number
+    }
+
+    function addNumber(address friendAddress) returns (uint newNumber){
+        //if(Numbers[friendAddress] == 0) throw;   //friend not stored, but don't want to lose gas if they haven't stored
+        if(friendAddress == msg.sender) throw;
+        //uint newNumber;
+        newNumber = Numbers[friendAddress] + Numbers[msg.sender];        //add looked up number to my number
+    }
+
+}
