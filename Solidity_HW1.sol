afcec1158f1b8d043c0641e47262c763e2716206
Jessica Marshall <jessica.marshall14@gmail.com>
2017-06-12 16:56:08 -0400

    added buyToken function

--- Solidity_HW1.sol
+++ Solidity_HW1.sol
@@ -6,20 +6,32 @@ contract JaesToken{
   mapping (address => mapping (address => uint)) allowed;
   //store the amount allowed by an address for another address to spend on its behalf
     uint256 public totalSupply;
-    uint8 public xChange;     //exchange rate
-    string tokenName;
+    uint8 public exchangeRate;     //exchange rate
+    string public tokenName;
+    address owner;      //deployer of contract
+    uint exchangeTemp;
+    uint numTokens;
+    uint amountWei;
 
-  function JaesToken(uint _initialAmount, string _tokenName){
+  function JaesToken(uint _initialAmount, string _tokenName) {
     //initialize this contract & mint tokens
-    balances[msg.sender] = _initialAmount;    //give contract creator all tokens
+    owner = msg.sender;
+    balances[owner] = _initialAmount;    //give contract creator all tokens
     totalSupply = _initialAmount;
     tokenName = _tokenName;
-    xChange = 10;   //1 ETH = 10 tokens
+    exchangeRate = 10;   //1 ETH = 10 tokens
   }
 
-  function buyToken(uint _amount) {
+  function buyToken(uint _amountEther){   //_amountEther is amount ether desired to pay
+      numTokens = _amountEther * 10;  //calculate num tokens
+      amountWei = _amountEther ** 18; //convert ether to wei
 
-    balances[msg.sender] += _amount;    //deposit token in account
+      if (balances[owner] < numTokens) throw;   //not enough tokens
+      if (balances[msg.sender] + numTokens < balances[msg.sender]) throw; //overflow
+      if (msg.sender.balance < amountWei) throw;    //can't afford tokens
+      if (owner.balance + amountWei <= owner.balance) throw;  //overflow
+      owner.transfer(amountWei);   //msg.sender transfers wei to owner
+      balances[msg.sender] += numTokens;    //deposit token in account
   }
 
   function totalSupply() constant returns (uint256 supply){
@@ -33,7 +45,7 @@ contract JaesToken{
 
   function transfer(address _recipient, uint256 _amount) returns (bool success){
     //transfer _amount tokens to address _recipient
-    if ((balances[_recipient] + _amount) < balances[_recipient])    //check for overflow
+    if ((balances[_recipient] + _amount) <= balances[_recipient])    //check for overflow
       return false;
 
     if (balances[msg.sender] >= _amount && _amount > 0) { //make sure sender has money to give
@@ -59,7 +71,6 @@ contract JaesToken{
     if(balances[_to] + _value < balances[_to]) throw;
     balances[_from] -= _value;
     balances[_to] += _value;
-
   }
 
 }
